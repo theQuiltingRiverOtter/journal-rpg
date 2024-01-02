6448d30bd4b0712f320d829abf1605695e151210
Megan Buck <thequiltingriverotter@gmail.com>
2024-01-02 10:56:03 -0500

    changed settings.py

--- back_end/journal_rpg_proj/settings.py
+++ back_end/journal_rpg_proj/settings.py
@@ -25,12 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1v5!e(iu2&^a1_fs06hx_jdp088e$!g=oueamm7%cm4^0wof%g"
+SECRET_KEY = env.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
 
 CORS_ALLOW_ALL_ORIGINS = True
 
